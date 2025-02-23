392013648f5c4f5cd14cee6dd86825ce473781a4
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-02-23 15:13:37 +0100

    tested some PHP basics

---
+++ 01.Preparation/01.PHP/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php                                       #php = Hypertext Preprocessor
+    echo "My first PHP script!";
+    echo "<br>"; //new line
+    $PHP = "PHP is fun!";
+    echo $PHP;
+    echo "<br>"; 
+
+    //Data Types
+    //Integer (Whole Numbers)
+    $Integer = 5; 
+    echo "Integer:" + $Integer;
+    echo "<br>"; 
+    //Float (Decimal Numbers)
+    $Float = 3.141;
+    echo $Float;
+    echo "<br>"; 
+    //Strings (text)
+    $String = "I'm a String!";
+    echo $String;
+    echo "<br>";
+    //Boolean (True/False)
+    $Boolean = true;
+    echo $Boolean;
+    $Boolean = false;
+    echo $Boolean;
+    ?>
+</body>
+</html>
